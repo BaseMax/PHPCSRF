b9478821310bc9350830758fbaf41b6e286e8731
aliiahmadi <aliahmadi.github@gmail.com>
2023-04-13 17:24:11 +0330

    validation of csrf token sended with hidden input

--- src/Controllers/Controller.php
+++ src/Controllers/Controller.php
@@ -2,10 +2,16 @@
 
 namespace App\Controllers;
 
+use App\Application\Application;
 
 class Controller
 {
     public function __construct()
     {
+    }
+
+    protected function getBody(): array
+    {
+        return Application::$app->request->getBody();
     }
 }
--- src/Controllers/LoginController.php
+++ src/Controllers/LoginController.php
@@ -14,6 +14,6 @@ class LoginController extends Controller
 
     public function store()
     {
-        return "here is store";
+        return $this->getBody();
     }
 }
