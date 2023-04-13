6c05230e839066fab6e37fcf7434906172a9c7bc
aliiahmadi <aliahmadi.github@gmail.com>
2023-04-13 14:49:40 +0330

    implement CSRF generate and store and validation

--- src/Application/CSRF.php
+++ src/Application/CSRF.php
@@ -5,11 +5,30 @@ namespace App\Application;
 
 class CSRF
 {
-    static public function generate()
+    static public function generate(): string
     {
+        return bin2hex(random_bytes(40));
     }
 
-    static public function validate()
+    static public function validate(string $csrf): bool
     {
+        $sessionCsrf = $_SESSION["CSRF_TOKEN"] ?? false;
+
+        if (!$sessionCsrf) {
+            return false;
+        }
+
+        if ($csrf !== $sessionCsrf) {
+            return false;
+        }
+
+        return true;
+    }
+
+    static public function storeSCRF(): bool
+    {
+        $_SESSION["CSRF_TOKEN"] = CSRF::generate();
+
+        return true;
     }
 }
--- src/Controllers/LoginController.php
+++ src/Controllers/LoginController.php
@@ -3,11 +3,15 @@
 namespace App\Controllers;
 
 use App\Application\ViewRender;
+use App\Application\CSRF;
 
 class LoginController extends Controller
 {
     public function show()
     {
+
+        var_dump(CSRF::generate());
+
         return ViewRender::render("login");
     }
 }
